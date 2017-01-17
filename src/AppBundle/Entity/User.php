9a48fbcb9e438ea0c804f92d517954a078d796df
MateuszPC <mateusz.mietnus95@gmail.com>
2017-01-17 12:58:11 +0100

    Dostosowanie koszyka do FOSUserBundle

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -5,6 +5,7 @@ namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @ORM\Entity
@@ -19,9 +20,56 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\Cart", mappedBy="user")
+     * @ORM\JoinColumn(name="id", referencedColumnName="userId")
+     */
+    private $cart;
+
+    /**
+     * @return mixed
+     */
+    public function getCart()
+    {
+        return $this->cart;
+    }
+
+    /**
+     * @param mixed $cart
+     */
+    public function setCart($cart)
+    {
+        $this->cart = $cart;
+    }
+
+    /**
+     * Add cart
+     *
+     * @param \AppBundle\Entity\Cart $cart
+     *
+     * @return User
+     */
+    public function addCart(\AppBundle\Entity\Cart $cart)
+    {
+        $this->cart[] = $cart;
+
+        return $this;
+    }
+
+    /**
+     * Remove cart
+     *
+     * @param \AppBundle\Entity\Cart $cart
+     */
+    public function removeCart(\AppBundle\Entity\Cart $cart)
+    {
+        $this->cart->removeElement($cart);
+    }
+
     public function __construct()
     {
         parent::__construct();
-        // your own logic
+        $this->cart = new ArrayCollection();
     }
+
 }
